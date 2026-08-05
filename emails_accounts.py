cad7ed85ffafde4925c01bcf9834f9afa2c200d6
developpeur <dev@vps-69bb4135.vps.ovh.net>
2026-08-05 19:24:13 +0000

    verife

--- emails_accounts.py
+++ emails_accounts.py
@@ -7,12 +7,12 @@ Note : Les adresses d'expédition (SENDER_EMAIL) doivent être validées sur vot
 MAIRIE_CONTACT_RECEPTION_EMAIL = "cizeautom@gmail.com"
 
 # acuses de reception
-MAIRIE_SENDER_EMAIL = "cizeautom@gmail.com"
+MAIRIE_SENDER_EMAIL = "mairie@dhuizon.fr"
 MAIRIE_SENDER_NAME = "Mairie de Dhuizon"
 
 # refuser ou accepter au centre de loisirs
-CENTRE_LOISIRS_SENDER_EMAIL = "cizeautom@gmail.com"
+CENTRE_LOISIRS_SENDER_EMAIL = "centre-de-loisirs@dhuizon.fr"
 CENTRE_LOISIRS_SENDER_NAME = "Centre de Loisirs de Dhuizon"
 
 # inscription garderie et cantine
-PERISCOLAIRE_RECEPTION_EMAIL = "cizeautom@gmail.com"
+PERISCOLAIRE_RECEPTION_EMAIL = "mairie@dhuizon.fr"
